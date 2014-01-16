6d1c335b146d6237e6e92cfe3bdc1fb3b769f329
Seiki Koga <alfr0475@gmail.com>
2014-01-16 15:42:46 +0900

    テスト用の設定定義をphpunit.xmlに移行。

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,9 +5,4 @@ define('ME_ROOT_DIR', realpath(dirname(__FILE__) . '/../'));
 define('ME_APP_DIR', ME_ROOT_DIR . '/app');
 define('ME_SYSTEM_DIR', ME_ROOT_DIR);
 
-define('TEST_DB_HOSTNAME', 'localhost');
-define('TEST_DB_USERNAME', 'travis');
-define('TEST_DB_PASSWORD', '');
-define('TEST_DB_DATABASE', 'meelia_test');
-
 require_once ME_ROOT_DIR . '/meelia.php';
